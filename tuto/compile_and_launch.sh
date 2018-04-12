78e841e1ecdf26ee408cad3c29a3a21aba014545
Nicolas Huguenin <nichuguen@gmail.com>
2018-04-12 18:09:28 +0200

    tuto base ok

---
+++ tuto/compile_and_launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# compile using maven
+mvn compile package
+
+# copy jar to /tmp/tuto
+cp target/gs-rest-service-0.1.0.jar ./server/java-server.jar
+
+# launch docker-compose
+docker-compose up --build
+
+# now go to http://localhost:8080/greeting?name=you
---
+++ tuto/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# launch docker-compose
+docker-compose up --build
+
+# now go to http://localhost:8080/greeting?name=you
