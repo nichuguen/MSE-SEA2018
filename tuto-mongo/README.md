828702f222909d04c32130c978a5aec4a9dbc023
Nicolas Huguenin <nichuguen@gmail.com>
2018-04-17 12:55:48 +0200

    working mongo

---
+++ tuto-mongo/README.md
@@ -0,0 +1,1 @@
+Take a look at [compile_and_launch.sh](compile_and_launch.sh) and [launch.sh](launch.sh) if everything is already compiled.
---
+++ tuto-mongo/compile_and_launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# compile using maven
+mvn compile package
+
+# copy jar to /tmp/tuto
+cp target/gs-accessing-mongodb-data-rest-0.1.0.jar ./server/java-server.jar
+
+# launch docker-compose
+# all the configuration is in docker-compose.yml
+docker-compose up --build
+
+# now go to http://localhost:8080/greeting?name=you
